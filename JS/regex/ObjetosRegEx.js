fb696b9ed9b01ab6d28d83d81b0a03133aa48340
rediiebr <josemanuelmartin.fuentezuelas@gmail.com>
2019-10-04 14:12:51 +0200

    update

--- JS/regex/ObjetosRegEx.js
+++ JS/regex/ObjetosRegEx.js
@@ -1,4 +1,16 @@
 /**
  *dni: /^(\d{8}[A-Z]$/ig)
- * 
+ * (^| )(\d+)( |$|\. )
+ * dni: /^(\d{8}[A-Z]$/ig)
+ * word:^((w| )+\.)+(doc|docx)$
+ * img:^((\w| )+\.)+(gif|jpg|png)$
+ * hex:[0-9A-F]+
+ * NOMBRE:/[A-ZÁÉÍÓÚÑ]{1}[a-záéíóúñ-]+\s?
+ * cadena empty:.
+ * cadena mayuscula: [A-Z]+
+ * ^(\d{8}[A-Z]$)
+ * un fono con o sin prefijo pero solo de espñaita:(\+[34]{1}|[0034])*([0-9]{9})
+ * MAC:^([0-9A-F]{2}[: \-]?){5}[0-9A-F]{2}$
+ * IP:^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]).){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$
  */
+
