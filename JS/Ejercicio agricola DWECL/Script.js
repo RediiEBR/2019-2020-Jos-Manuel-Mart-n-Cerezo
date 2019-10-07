d6e29dd1445d1cfcbf4c662e277772e139a2ff50
rediiebr <josemanuelmartin.fuentezuelas@gmail.com>
2019-10-07 13:43:23 +0200

    13:42 dwecl

---
+++ JS/Ejercicio agricola DWECL/Script.js
@@ -0,0 +1,6 @@
+//hacer lo de antonio con silverio
+/**
+ * agricultor:dni,nombre,apellidos,email
+ * parcela:ID,nombre(required),nº parcela,nºpoligono,nºolivos
+ * actividades:id,titulo,tipo(labor o tratamiento),descripcion(max 500 char) y fecha
+ */
