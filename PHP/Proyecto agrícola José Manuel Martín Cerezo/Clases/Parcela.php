e90f835bbdad2539c9ba46464c7a948052ce548e
rediiebr <josemanuelmartin.fuentezuelas@gmail.com>
2019-10-09 20:49:38 +0200

    2049

--- PHP/Proyecto agrícola José Manuel Martín Cerezo/Clases/Parcela.php
+++ PHP/Proyecto agrícola José Manuel Martín Cerezo/Clases/Parcela.php
@@ -16,6 +16,106 @@
             $this->numOlivos=$numOlivos;
             
         }
+
+        /**
+         * Get the value of idParcela
+         */ 
+        public function getIdParcela()
+        {
+                return $this->idParcela;
+        }
+
+        /**
+         * Set the value of idParcela
+         *
+         * @return  self
+         */ 
+        public function setIdParcela($idParcela)
+        {
+                $this->idParcela = $idParcela;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of nombre
+         */ 
+        public function getNombre()
+        {
+                return $this->nombre;
+        }
+
+        /**
+         * Set the value of nombre
+         *
+         * @return  self
+         */ 
+        public function setNombre($nombre)
+        {
+                $this->nombre = $nombre;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of numParcela
+         */ 
+        public function getNumParcela()
+        {
+                return $this->numParcela;
+        }
+
+        /**
+         * Set the value of numParcela
+         *
+         * @return  self
+         */ 
+        public function setNumParcela($numParcela)
+        {
+                $this->numParcela = $numParcela;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of numPoligono
+         */ 
+        public function getNumPoligono()
+        {
+                return $this->numPoligono;
+        }
+
+        /**
+         * Set the value of numPoligono
+         *
+         * @return  self
+         */ 
+        public function setNumPoligono($numPoligono)
+        {
+                $this->numPoligono = $numPoligono;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of numOlivos
+         */ 
+        public function getNumOlivos()
+        {
+                return $this->numOlivos;
+        }
+
+        /**
+         * Set the value of numOlivos
+         *
+         * @return  self
+         */ 
+        public function setNumOlivos($numOlivos)
+        {
+                $this->numOlivos = $numOlivos;
+
+                return $this;
+        }
     }
     
 ?>
