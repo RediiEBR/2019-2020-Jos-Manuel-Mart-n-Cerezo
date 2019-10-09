201eb0cfc6cfadae6692ecf2bd9746f1a99e37d6
rediiebr <josemanuelmartin.fuentezuelas@gmail.com>
2019-10-09 08:15:32 +0200

    8

--- JS/3EnRayaPorSilverio/3enRaya.js
+++ JS/3EnRayaPorSilverio/3enRaya.js
@@ -1,4 +1,4 @@
-"strict mode";
+
 /**
  * Juego tres en raya
  * 
@@ -16,11 +16,26 @@ TresEnRaya.prototype.jugar()=function(f,c)
     if(this.juego[f][c]===undefined)
     {
         this.juego[f][c]=this.turno;
+        dibujar();
         this.turno=(this.turno+1)%2;
+        
         //comprobar si hay victoria
     }
 };
-TresEnRaya.prototype.victoria=function(){};//codigo para comprobar victoria:true\false
-var j = new TresEnRaya("Juan","Antonio");
-j.jugar(0,0);
-j.jugar(2,2);
+TresEnRaya.prototype.victoria=function(){
+    if(this.juego==[[],[],[]])
+    {
+
+    }
+};//codigo para comprobar victoria:true\false
+
+ function dibujarTablero()
+ {
+     //debugger;
+     document.write("<table border='1' style='width:300px;height:300px'>"+"<tr>"+"<td onclick=''></td>"+"<td></td>"+"<td></td>"+"</tr>"+"<tr>"+"<td></td>"+"<td></td>"+"<td></td>"+"</tr>"+"<tr>"+"<td></td>"+"<td></td>"+"<td></td>"+"</tr>"+"</table>");
+    
+ }
+ function dibujar()
+ {
+
+ }
